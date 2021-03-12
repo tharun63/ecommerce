2e86fc3ede64cf6e17dacf2d7f4e125d7111b638
Tharun <tharunrao063@gmail.com>
2021-03-12 22:20:19 +0530

    Add files via upload

---
+++ models/category.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+//const { stringify } = require('querystring')
+
+const categorySchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+            maxlength: 32
+        }
+
+    },
+    { timestamps: true }
+);
+
+
+
+
+module.exports = mongoose.model('Category', categorySchema);
